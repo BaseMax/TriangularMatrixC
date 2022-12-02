b9b4bb9eb66f0fd465976f6b10404a337bd6e3f6
Max Base <maxbasecode@gmail.com>
2022-12-02 17:48:58 +0100

    create c and header file

---
+++ TriangularMatrix.c
@@ -0,0 +1,12 @@
+/**
+ * @file TriangularMatrix.c
+ * @author Max Base (maxbasecode@gmail.com)
+ * @brief Working with large triangular matrix (upper or lower) in C
+ * @version 0.1
+ * @date 2022-12-02
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "TriangularMatrix.h"
+
---
+++ TriangularMatrix.h
@@ -0,0 +1,13 @@
+#ifndef _TRIANGULARMATRIX_H_
+#define _TRIANGULARMATRIX_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TriangularMatrix
+{
+    int n;
+    int *A;
+} TriangularMatrix;
+
+#endif
